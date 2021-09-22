db22d904c8afadc7067015d596ee90801acb5ea7
Maximiliano Mercado Diaz <50929240+maximarsoficial@users.noreply.github.com>
2021-09-22 10:27:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const app = express();
+
+app.get('/' , (req,res) =>{
+    res.json({
+        hello: 'incio'
+    } );
+} );
+
+
+app.post('/' , (req,res) =>{
+    res.json({
+        hello: 'vista del post'
+    } );
+} );
+
+app.listen(3000, () =>{
+    console.log('servidor levantado en el puerto 3000')
+} )
+
